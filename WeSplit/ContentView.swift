1cf3e915247b507f46449a0f71b0dccc96dead38
Sofia Andrade <sandrade@lcs.on.ca>
2020-11-09 12:22:53 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sofia Andrade on 9/11/20.
+//  Copyright © 2020 Sofia Andrade. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
